16b7a52bffa8e7b45234d8ed21690b85bf852427
youngenjoy <61712822@qq.com>
2017-02-16 11:23:54 +0800

    测试新加代码

--- src/main/java/com/siebre/test/Test.java
+++ src/main/java/com/siebre/test/Test.java
@@ -4,4 +4,7 @@ package com.siebre.test;
  * Created by Administrator on 2017/02/15 0015.
  */
 public class Test {
+    public static void main(String args[]){
+        System.out.println("新加");
+    }
 }
